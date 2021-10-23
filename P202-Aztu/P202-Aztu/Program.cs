c29182bd73bcd8212c97c24c391969de827e73cf
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-10-23 07:38:22 +0400

    Add files via upload

---
+++ P202-Aztu/P202-Aztu/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace P202_Aztu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variable
+            //int age = 19;
+            //age = 20;
+            //int number = 21;
+            //string word = "Hello";
+            //word = "13";
+            //char letter= 'a';
+            //decimal pi = 3.14m;
+            ////console.writeline(number);
+            //long view = 246154613;
+            //var a = true;
+            //dynamic x = 15;
+            //Console.WriteLine(x);
+            //x = "Seymur";
+            //x = true;
+            //Console.WriteLine(x);
+            #endregion
+
+            #region Control Structure
+            #region Condition
+            //int age = 16;
+            //if (age>16)
+            //{
+
+            //    Console.WriteLine("Welcome");
+            //}
+            //else if (age == 16)
+            //{
+            //    Console.WriteLine("16");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bye");
+            //}
+
+            //int day = 50;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //    case 5:
+            //    case 6:
+            //    case 7:
+            //        Console.WriteLine("Other day");
+            //        break;
+            //    default:
+            //        Console.WriteLine("wrong number");
+            //        break;
+            //}
+            #endregion
+
+            #region Loops
+
+            Console.WriteLine("Yashi daxil edin:");
+            string result = Console.ReadLine();
+            int age = int.Parse(result);
+            Console.WriteLine("yashiniz: "+age);
+            //int[] arr = { 1, 2, 3, 4, 5 };
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //foreach (int item in arr)
+            //{
+            //    if(item%2==0) 
+            //        Console.WriteLine(item);
+            //}
+            //int i = 1;
+
+
+            //while (true)
+            //{
+
+            //    i++;
+            //    if (i == 5)
+            //    {
+            //        continue;
+            //    }
+
+            //    Console.WriteLine(i);
+
+            //}
+
+            //for (;;)
+            //{
+
+            //}
+
+            //do 
+            //{
+            //    Console.WriteLine(i);
+            //} while (i < 1);
+
+            //int sum = 0;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            //for (int i = 0; i < 100; i++)
+            //{
+            //}
+            #endregion
+            #endregion
+        }
+    }
+}
